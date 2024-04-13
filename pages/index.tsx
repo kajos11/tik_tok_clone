1f95172576dbba1dd50f4d77fe7ca84a3eaf68f8
Karthik <joshi.karthik@yahoo.com>
2024-04-13 08:09:17 +0530

    Tik Tok Clone

---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+import { NextPage } from "next";
+
+
+const Home: NextPage = () =>{
+    return (
+        <div className="text-3xl font-bold underline">
+            Tik Tok Clone App!!@
+        </div>
+    )
+}
+
+export default Home;
---
+++ sanity-backend/sanity.cli.js
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: 'mckl9h6d',
+    dataset: 'production'
+  }
+})
